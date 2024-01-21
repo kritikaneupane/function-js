b584258780c9d83b5c1bf61d35beff34f231b08d
Your Name <neupanekritika49@gmail.com>
2024-01-21 14:44:08 +0545

    learning form

---
+++ script.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5];
+arr.forEach((val) => {
+  console.log(val);
+});
+//console.log?(val.toUpperCase());// value alphabet
+// for a given array of numbers,print the square of each value using the forEach loop.
